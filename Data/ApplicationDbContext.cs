49d3046d4d1d8c30c18d10b9c1761225c1f3c7f5
tyler <tyler.mcintyre404@gmail.com>
2022-07-25 19:37:19 -0400

    Entity Framework Database Setup
    Added Entity Framework to the project and created the connection string to the SQL database.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Battler.Models;
+
+namespace MVC_Battler.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Character> Characters { get; set; }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Battler.Models;
+
+public class Character
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public int HP { get; set; }
+
+    public Character()
+    {
+        
+    }
+}
